d7c4c0a5b3aa1d848dbbc7aae239e4bf6595f6ce
云鹤 <71846342+cloudcranes@users.noreply.github.com>
2020-11-02 23:15:21 +0800

    IDEALHX

--- src/cn/cqswxy/test/HelloWorld.java
+++ src/cn/cqswxy/test/HelloWorld.java
@@ -2,10 +2,10 @@ package cn.cqswxy.test;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.out.println("Hello,World!");
+        System.out.println("Hello,World!");
 
-	}
+    }
 
 }
---
+++ src/cn/cqswxy/test/Test.java
@@ -0,0 +1,9 @@
+package cn.cqswxy.test;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("这是测试IDEA");
+    }
+
+}
